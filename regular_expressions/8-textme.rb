476d34fa630b5a33ce876899b7ccef9e44879b47
makuachteny <m.gatluak@alustudent.com>
2023-01-12 03:07:22 -0800

    Fixed bug

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,7 +1,4 @@
 #!/usr/bin/env ruby
-log_line = ARGV[0]
-sender = log_line[/from:(\S+)/, 1]
-receiver = log_line[/to:(\S+)/, 1]
-flags = log_line[/flags:(\S+)/, 1]
-
-puts "#{sender},#{receiver},#{flags}"
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
