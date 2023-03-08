eac5c91203b693913a7973d539f0f5815841a860
makuachteny <m.gatluak@alustudent.com>
2023-03-08 16:12:29 +0300

    hot fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-
-""" Script that fetch the number of subscribers for a given subreddit."""
+"""Script that fetch number of subscriber for a given subreddit."""
 import requests
 
 
-def number_of_subcribers(subreddit):
-    """Set the HTTP headers for the request to the Reddit API"""
+def number_of_subscribers(subreddit):
+    """Return number of subscribers is @subreddit is valid subreddit.
+    if not return 0."""
 
-    headers = {'User-Agent': 'm_tenyg'}
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        subscriber_number = (
-            json_data.get('data')
-            .get('children')[0]
-            .get('data')
-            .get('subreddit_subscribers')
-        )
+        subscriber_number = json_data.get('data').get(
+            'children')[0].get('data').get('subreddit_subscribers')
         return subscriber_number
     else:
         return 0
