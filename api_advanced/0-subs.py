2ce9fda3aae613568523dd3c552dcb30aa6cc7db
makuachteny <m.gatluak@alustudent.com>
2023-03-08 10:15:58 +0300

    set user-Agent

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,18 +3,18 @@
 """ Script that fetch the number of subscribers (not active users, total subscribers) for a given subreddit."""
 import requests
 
-def number_of_subcribers(subreddit):
-	"""Returns the number of subscribers for a given subreddit, if it is a valid subreddit.
-	If the subreddit is invalid or cannot be accessed, returns 0."""
-	
-	headers = {'User-Agent': 'MyAPI/0.0.1'}
-	subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-	response = requests.get(subreddit_url, headers=headers)
 
-	if response.status_code == 200:
-		json_data = response.json()
-		subscriber_number = json_data.get('data').get(
-			'children')[0].get('data').get('subreddit_subscribers')
-		return subscriber_number
-	else:
-            return 0
+def number_of_subcribers(subreddit):
+    """Set the HTTP headers for the request to the Reddit API"""
+
+    headers = {'User-Agent': 'Scrapping_subscribers '}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        subscriber_number = json_data.get('data').get(
+            'children')[0].get('data').get('subreddit_subscribers')
+        return subscriber_number
+    else:
+        return 0
